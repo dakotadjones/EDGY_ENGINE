7d7fbc627bd740a2ec0edb095171f5fe45ddbc3e
Emily Bartman <bartman.em@gmail.com>
2016-03-16 14:11:08 -0700

    Started file upload (requires localhost)

--- engine/mapgen.js
+++ engine/mapgen.js
@@ -1,23 +1,36 @@
 // mapgen.js
 //var mapgrid = document.getElementById("");
 
-$(document).on("click", "#upload", function() {
+function uploadJson() {
 	var json_file_data = $("#json").prop("files")[0];
 	var form_data = new FormData();// Creating object of FormData class
 	form_data.append("file", json_file_data) // Appending parameter named file with properties of file_field to form_data
-	form_data.append("user_id", 123) // Adding extra parameters to form_data
-	console.log("hello");
 	$.ajax({
-                url: "uploads/",
+                url: "upload.php",
                 dataType: 'json',
                 cache: false,
                 contentType: false,
                 processData: false,
                 data: form_data,                         // Setting the data attribute of ajax with file_data
                 type: 'post',
-				success: function() { alert("success"); },
-				complete: function() { console.log("complete");}
+				complete: function() { console.log("completed upload");}
 				
-       })
-	
-});
+       });
+
+}
+
+function uploadPng() {
+	var png_file_data = $("#png").prop("files")[0];
+	var form_data = new FormData();// Creating object of FormData class
+	form_data.append("file", png_file_data) // Appending parameter named file with properties of file_field to form_data
+	$.ajax({
+                url: "upload.php",
+                cache: false,
+                contentType: false,
+                processData: false,
+                data: form_data,                         // Setting the data attribute of ajax with file_data
+                type: 'post',
+				complete: function() { console.log("completed upload");}
+				
+       });
+}
