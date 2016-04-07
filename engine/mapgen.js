d1e0deae4f40f401780d8d9908730ef3c13dcd40
Emily Bartman <bartman.em@gmail.com>
2016-04-06 17:06:43 -0700

    Finished map object creation and ui

--- engine/mapgen.js
+++ engine/mapgen.js
@@ -5,28 +5,25 @@
 var checkedDirectory = false;
 var c = document.getElementById("map");
 var ctx = c.getContext("2d");
-ctx.strokeStyle = "#919191";
+
+var gridColor = "#919191"
+var definedColor = "#a6a6a6";
+var fullyDefinedColor = "#aedba4";
+var fullyDefinedStroke = "#5db649";
+var selectColor = "#ED686E";
+var darkSelect = "#8F383C";
+var selectStroke = "#6e2b2e" 
+var wallColor = "#4d4d4d";
+var selectedWallColor = "#5c2427";
+
+ctx.strokeStyle = gridColor;
 ctx.lineWidth = 2;
 var square = 20;
 var devPackChosen;
 
 var map; // JSON containing your new map :) 
 
-var currCords = [];
-
-// could be greatly expanded
-var colorMap = {
-	"blue":"#003399",
-	"grass":"#006633",
-	"ground":"#604020",
-	"brown":"#604020",
-	"dirt":"#604020",
-	"purple":"#993399",
-	"brick":"#cc3300",
-	"sky":"#003399",
-	"sand":"#ffd633",
-	"default":"#66ff66"
-}
+var currCoords = [];
 
 function uploadJson() {
 	var json_file_data = $("#json").prop("files")[0];
@@ -63,9 +60,7 @@ function uploadPng() {
                 type: 'post',
 				complete: function(data) { 
 					$("#uploaded-file-png").val(data.responseText);
-					if ($("#uploaded-file-json").val().length) {
-						$("#parse").show();
-					}
+					if ($("#uploaded-file-json").val().length) { $("#parse").show(); }
 				}
 				
        });
@@ -73,9 +68,7 @@ function uploadPng() {
 
 function hideChoices(id, callback) {
 	$(id).hide("slide", 500);
-	if (typeof callback !== 'undefined') {
-		 setTimeout(callback, 500);
-	}
+	if (typeof callback !== 'undefined') { setTimeout(callback, 500);}
 }
 
 function showChoices(id) {
@@ -112,22 +105,18 @@ function savePack(filename) {
 }
 
 function drawBaseMap(width, height) {
-	if (map === undefined) {
-		map = {"player":{"x":1,"y":1,"facing":"east"}, "characters":[]};
-	}
+	if (map === undefined) { map = {"player":{"x":1,"y":1,"facing":"east"}, "characters":[]}; }
 	for (var h = 1; h <= height; h++) {
 		for (var w = 1; w <= width; w++) {
 			var coord = w.toString()+","+h.toString();
 			
 			if (!map.hasOwnProperty(coord)) { map[coord] = {"ceil":null,"floor":null,"north":null,"south":null,"east":null,"west":null}; } 
-			if(map.player.x == w && map.player.y == h) { 
-				drawPlayer(w, h);
-			}
+			if(map.player.x == w && map.player.y == h) { drawPlayer(w, h); }
 			
 			ctx.rect(w*square, h*square, square, square);
 		}
 	}
-	ctx.strokeStyle = "#919191";
+	ctx.strokeStyle = gridColor;
 	ctx.lineWidth = 2;
 	ctx.stroke();
 	
@@ -138,6 +127,39 @@ function drawBaseMap(width, height) {
 function drawPlayer(x, y) {
 	ctx.fillStyle="black"; 
 	ctx.fillRect(x*square, y*square, square, square); 
+}
+
+function drawDefined(x, y, color) {
+	ctx.fillStyle = color;
+	ctx.fillRect(x*square,y*square,square,square);
+}
+
+function drawWalls(x, y, walls, color) {
+	ctx.strokeStyle = color;
+	ctx.lineWidth = 2;
+	ctx.beginPath();
+	for (var w in walls) {
+		switch(walls[w]) {
+			case "north":
+				ctx.moveTo(x*square, y*square+1);
+				ctx.lineTo(x*square + square, y*square+1)
+				break;
+			case "south":
+				ctx.moveTo(x*square, (y*square+square)-1);
+				ctx.lineTo(x*square + square, (y*square+square)-1)
+				break;
+			case "east":
+				ctx.moveTo((x*square + square)-1, y*square);
+				ctx.lineTo((x*square + square)-1, (y*square +square));
+				break;
+			case "west":
+				ctx.moveTo(x*square+1, (y*square));
+				ctx.lineTo(x*square+1, (y*square + square));
+				break;
+		}
+	}
+	ctx.stroke();
+	
 }
 
 function destroyMap() {	
@@ -163,6 +185,28 @@ function openSelector(e) {
     });	
 }
 
+$("#map").mousemove(function(e) {
+	var cx = Math.ceil(e.offsetX/square)-1;
+	var cy = Math.ceil(e.offsetY/square)-1;
+	var coord = cx.toString()+","+cy.toString();
+	$("#coordinate-info").val(coord);
+	if (map !== undefined && map[coord] !== undefined) {
+		if (map[coord]["north"]) { $("#north-info").val(map[coord]["north"]); }
+		else { $("#north-info").val("Null"); }
+		if (map[coord]["south"]) { $("#south-info").val(map[coord]["south"]); }
+		else { $("#south-info").val("Null"); }
+		if (map[coord]["east"]) { $("#east-info").val(map[coord]["east"]); }
+		else { $("#east-info").val("Null"); }
+		if (map[coord]["west"]) { $("#west-info").val(map[coord]["west"]); }
+		else { $("#west-info").val("Null"); }
+		if (map[coord]["ceil"]) { $("#ceiling-info").val(map[coord]["ceil"]); }
+		else { $("#ceiling-info").val("Null"); }
+		if (map[coord]["floor"]) { $("#floor-info").val(map[coord]["floor"]); }
+		else { $("#floor-info").val("Null"); }
+	}
+	
+})
+
 function selectElements() {
     $("#map").unbind("mousemove", openSelector);
     $("#map").unbind("mouseup", selectElements);
@@ -179,8 +223,9 @@ function selectElements() {
 	var totalHeight = Math.abs(t-h);
 	var totalWidth = Math.abs(l-w);
 	
-	drawSelectedMap(l, t, totalWidth, totalHeight);
-	
+	if (!recentlyCleared) {
+		drawSelectedMap(l, t, totalWidth, totalHeight);
+	}
     $(".ghost-select").removeClass("ghost-active");
     $(".ghost-select").width(0).height(0);
 }
@@ -191,25 +236,41 @@ function resetPatterns() {
 }
 
 function setCurrentSelectedCoord(x, y, id) {
-	if (!$(id).text().length) {
-		$(id).append("Selected Coords (x, y)<br>")
-	}
+	if (!$(id).text().length) {	$(id).append("Selected Coords (x, y)<br>"); }
 	$(id).append(x.toString() +",\t\t"+ y.toString() + "<br>")
-	currCords.push(x.toString()+","+y.toString());
+	currCoords.push(x.toString()+","+y.toString());
 }
 
 function eraseLastSelection(x,y,w,h) {
 	ctx.clearRect(x,y,w,h);
-	ctx.strokeStyle = "#919191";
+	ctx.strokeStyle = gridColor;
 	ctx.lineWidth = 2;
 	for (var i = x; i < x+w; i+=square) {
 			for (var j = y; j < h+y; j+=square) {
 				var sx = i/square;
 				var sy = j/square; 
-				if (map.player.x == sx && map.player.y == sy) {
-					drawPlayer(sx, sy)
+				if (floorAndCeilNotNull(map[sx.toString()+","+sy.toString()])) { 
+					ctx.strokeStyle = fullyDefinedStroke;
+					drawDefined(sx, sy, fullyDefinedColor); 
 				}
+				else if (floorNotNull(map[sx.toString()+","+sy.toString()]) || ceilingNotNull(map[sx.toString()+","+sy.toString()])) { 
+					ctx.strokeStyle = gridColor;
+					drawDefined(sx, sy, definedColor); 
+				} else {
+					ctx.strokeStyle = gridColor;
+				}
+				
+				if (map.player.x == sx && map.player.y == sy) { drawPlayer(sx, sy); }				
 				ctx.strokeRect(i, j, square, square);
+			}
+	}
+	
+	for (var k = x-square; k < x+w+1; k+=square) {
+			for (var m = y-square; m < h+y+1; m+=square) {
+				var kx = k/square;
+				var my = m/square; 		
+				var wallsToDraw = getWalls(map[kx.toString()+","+my.toString()]);
+				if (wallsToDraw.length > 0) { drawWalls(kx, my, wallsToDraw, wallColor); }
 			}
 	}
 	
@@ -234,27 +295,30 @@ function drawSelectedMap(x, y, w, h) {
 		if (cy == 0) cy = square;
 		
 		// optimize
-		if (lastRect !== undefined) {
-			eraseLastSelection(lastRect.x, lastRect.y, lastRect.w, lastRect.h);
-		}
+		if (lastRect !== undefined) { eraseLastSelection(lastRect.x, lastRect.y, lastRect.w, lastRect.h); }
 		ctx.clearRect(cx, cy, cw, ch);
-		ctx.fillStyle = "#ED686E";
+		ctx.fillStyle = selectColor;
 		ctx.fillRect(cx, cy, cw, ch);
 		$("#selected-boxes").empty();
-		currCords = [];
-		ctx.strokeStyle = "#000000";
-		ctx.lineWidth = 1;
+		currCoords = [];
+		
 		for (var i = cx; i < cw+cx; i+=square) {
 			for (var j = cy; j < ch+cy; j+=square) {
 				var sx = i/square;
 				var sy = j/square; 
-				if (map.player.x == sx && map.player.y == sy) {
-					drawPlayer(sx, sy)
-				}
+				if (floorNotNull(map[sx.toString()+","+sy.toString()]) || ceilingNotNull(map[sx.toString()+","+sy.toString()])) { drawDefined(sx, sy, darkSelect); }
+				if (map.player.x == sx && map.player.y == sy) {	drawPlayer(sx, sy); }
 				setCurrentSelectedCoord(sx, sy, "#selected-boxes");				
+				
+				ctx.strokeStyle = selectStroke;
+				ctx.lineWidth = 1;
 				ctx.strokeRect(i, j, square, square);
+				
+				var wallsToDraw = getWalls(map[sx.toString()+","+sy.toString()]);
+				if (wallsToDraw.length > 0) { drawWalls(sx, sy, wallsToDraw, selectedWallColor);}
 			}
 		}
+
 		lastRect = {"x":cx, "y":cy, "w":cw, "h":ch};
 	}
 }
@@ -262,6 +326,31 @@ function drawSelectedMap(x, y, w, h) {
 function isDefined(element, index, array) {
 	return element !== undefined; 
 }
+
+function floorNotNull(coordObj) {
+	return coordObj["floor"] != null;
+}
+
+function ceilingNotNull(coordObj) {
+	return coordObj["ceil"] != null;
+}
+
+function floorAndCeilNotNull(coordObj) {
+	return ceilingNotNull(coordObj) && floorNotNull(coordObj);
+}
+
+function getWalls(coordObj) {
+	var walls = [];
+	if (coordObj !== undefined) {
+		if (coordObj["north"] != null) { walls.push("north"); } 
+		if (coordObj["south"] != null) { walls.push ("south"); } 
+		if (coordObj["east"] != null) { walls.push("east"); }
+		if (coordObj["west"] != null) { walls.push("west"); }
+	}
+	return walls;
+}
+
+
 
 function getAvailablePatterns(packJson) {
 	var rObj = {"ceilings":[], "floors":[], "walls":[] };
@@ -306,6 +395,7 @@ $("#parse").click( function() {
 });
 
 $("#map-size").click(function () {
+	recentlyCleared = false;
 	var w = parseInt($("#map-width").val());
 	var h = parseInt($("#map-height").val());
 	if (w <= c.width/square && h <= c.height/square && !(isNaN(w) || isNaN(h)) ) {
@@ -344,9 +434,11 @@ $("#developer-choices").on("click", "button", function() {
 	request.send();
 });
 
+var recentlyCleared = false;
 $("#clear").click(function() { 
 	destroyMap(); 
 	resetPatterns();
+	recentlyCleared = true;
 	map = {"player":{"x":1,"y":1,"facing":"east"}, "characters":[]};
 });
 
@@ -370,7 +462,7 @@ $(".tileRep").click(function() {
 	var id = $(this).attr('id');
 	$(this).addClass("selectedTile").siblings(".tileRep").removeClass("selectedTile");
 	var availablePatterns = getAvailablePatterns(pack);
-	if (currCords.length > 0) $("#apply-pattern").removeClass("greyed");
+	if (currCoords.length > 0) $("#apply-pattern").removeClass("greyed");
 	$("#package-options").html("<option value='null'>None</option>");
 	switch(id) {
 		case "north":
@@ -386,7 +478,7 @@ $(".tileRep").click(function() {
 				$("#package-options").append("<option value='"+v+"'>"+v+"</option>");			
 			})
 			break;
-		case "ceiling":
+		case "ceil":
 			$(availablePatterns.ceilings).each(function(i,v) {
 				$("#package-options").append("<option value='"+v+"'>"+v+"</option>");			
 			})
@@ -397,10 +489,32 @@ $(".tileRep").click(function() {
 $("#apply-pattern").click(function() {
 	var surface = $(".tileRep.selectedTile").attr('id');
 	var texture = $("#package-options").val();
-	if (texture == "null") texture = null;
+	if (texture == "null") { texture = null; }
 	if(!$(this).hasClass("greyed")) {
-		for (var c in currCords) {
-			map[currCords[c]][surface] = texture;
+		for (var c in currCoords) {
+			map[currCoords[c]][surface] = texture;
+			
+			// feedback after map changes
+			if (floorNotNull(map[currCoords[c]]) || ceilingNotNull(map[currCoords[c]])) {
+				var x = parseInt(currCoords[c].split(',')[0]);
+				var y = parseInt(currCoords[c].split(',')[1]);
+				drawDefined(x,y, darkSelect);
+				ctx.strokeStyle = selectStroke;
+				ctx.lineWidth = 1;
+				ctx.strokeRect(x*square, y*square, square, square);
+				
+			} 
+			else {
+				var x = parseInt(currCoords[c].split(',')[0]);
+				var y = parseInt(currCoords[c].split(',')[1]);
+				drawDefined(x,y, selectColor);
+				ctx.strokeStyle = selectStroke;
+				ctx.lineWidth = 1;
+				ctx.strokeRect(x*square, y*square, square, square);
+			}
+			if (map.player.x == x && map.player.y == y) { drawPlayer(x, y);}
+			var wallsToDraw = getWalls(map[currCoords[c]]);
+			if (wallsToDraw.length > 0) { drawWalls(x, y, wallsToDraw, selectedWallColor);}
 		}
 	}
 });
