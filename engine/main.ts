3a6bef734bb9ef200fa80036b4599a5d4e06d9cd
WAFFO <garrettsteffen@gmail.com>
2016-04-07 15:12:26 -0700

    Merge

--- engine/main.ts
+++ engine/main.ts
@@ -1,6 +1,6 @@
 /// <reference path="Engine.ts" />
-var SRC = 'assets/package';
-var MAPSRC = 'assets/map_courtyard_grass.json'
+var SRC = 'assets/painted_pack';
+var MAPSRC = 'assets/map_courtyard.json'
 var pack;
 var map;
 var edgy;
